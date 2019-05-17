26597a0720f36bad1a016056909a0edc5fdfd1d0
atticus-reznor <50266059+atticus-reznor@users.noreply.github.com>
2019-05-17 20:16:14 +0100

    Create custom2.js

---
+++ js/custom2.js
@@ -0,0 +1,21 @@
+/*custom*/
+	/*
+$(document).ready(function($){
+    
+orientationChange();
+
+});
+
+function orientationChange() {
+    
+if(window.addEventListener) {
+        
+window.addEventListener("orientationchange", function() {
+            
+location.reload();
+        
+});
+    
+}
+
+}
